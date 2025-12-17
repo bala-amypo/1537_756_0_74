cfc8adfcff08f0b402d846a3a1aa32c4c7e76744
bala-amypo <bala@amypo.in>
2025-12-17 10:10:20 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationService.java
+++ src/main/java/com/example/demo/service/LocationService.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import java.util.List;
 
 import com.example.demo.entity.LocationEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface LocationService {
     LocationEntity createlocation(LocationEntity locationEntity);
     List<LocationEntity>getallLocation();
